a1347a6354cf6211de6994435dcb6a4270ec4888
Oscar Eduardo Fonseca Gomez <edsphinx@gmail.com>
2019-07-12 11:44:49 -0600

    commit before start integration tests

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
     state = { comment: '' };
@@ -10,24 +12,29 @@ class CommentBox extends Component {
     handleSubmit = e => {
         e.preventDefault();
         
-        //TODO: Call an action creator
-        //And save the comment
+        this.props.saveComment(this.state.comment);
+
         this.setState({ comment: '' });
     };
 
     render() {
         return (
-            <form className='ui form' onSubmit={this.handleSubmit}>
-                <div className = 'field'>
-                    <h4>Add a Comment</h4>
-                    <textarea onChange={this.handleChange} value={this.state.comment} />
-                </div>
-                <div>
-                    <button className='ui button'>Submit Comment</button>
-                </div>
-            </form>
+            <div>
+                <form className='ui form' onSubmit={this.handleSubmit}>
+                    <div className = 'ui field'>
+                        <h4>Add a Comment</h4>
+                        <textarea onChange={this.handleChange} value={this.state.comment} />
+                    </div>
+                    <div>
+                        <button className='ui button'>Submit Comment</button>
+                    </div>
+                </form>
+                <br></br>
+                <button className='ui button' onClick={this.props.fetchComments}>Fetch Comments</button>
+            </div>
+                
         );
     }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
