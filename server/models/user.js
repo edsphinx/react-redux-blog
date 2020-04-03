57cd4f9ed361140e602c38004796c778a0f5d819
Eduardo Fonseca <ofonseca@luminode.com>
2020-04-03 13:44:42 -0600

    server authentication added

--- server/models/user.js
+++ server/models/user.js
@@ -18,10 +18,18 @@ userSchema.pre('save', function(next) {
             if (err) { return next(err); }
 
             user.password = hash;
-            next();
-        })
+            next(); 
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) { return callback(err); }
+
+        callback(null, isMatch);
     })
-})
+}
 
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
